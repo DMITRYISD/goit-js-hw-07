c1cf4afb0277927266ae11cacebfea7140b3b70b
DMITRYISD <83534928+DMITRYISD@users.noreply.github.com>
2021-09-12 21:45:58 +0300

    Delete src directory

--- src/js/task-06.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("validation-input");
-input.addEventListener("blur", inputAreaLe);
-function inputAreaLe(event) {
-  const lengthValue = this.dataset.length;
-  event.currentTarget.value.length === Number(lengthValue)
-    ? (input.classList = "valid")
-    : (input.classList = "invalid");
-}
--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputRange = document.querySelector("#font-size-control");
-const inputText = document.querySelector("#text");
-
-const handleInput = (event) => {
-  inputText.style.fontSize = event.currentTarget.value + `px`;
-};
-
-inputRange.addEventListener("input", handleInput);
